60be6fc48bf129b091dfdba1a7cd2916880cfeef
Sarah <70615195+smanter82@users.noreply.github.com>
2021-02-10 21:05:00 -0500

    procfile

--- public/api.js
+++ public/api.js
@@ -24,7 +24,6 @@ const API = {
     return json;
   },
   async createWorkout(data = {}) {
-    console.log(data);
     const res = await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(data),
